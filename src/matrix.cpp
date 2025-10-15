14c46a6349fe0e1b16e65ad92badc5b78b6f4a50
Sreichtt <s.reicht@icloud.com>
2025-10-15 15:40:36 +0200

    bitte bitte

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -2,3 +2,20 @@
 #include <cmath>
 #include "vector.hpp"
 
+int main{
+    Matrix<double, RowMajor> A(2,2);
+    for (int i = 0; i<2;i++){
+        for (int k = 0;k<2, k++){
+            A(i,k) = i*k;
+        }
+    }
+
+    Vector<double> v(2);
+    for (int i = 0; i<v.Size();i++){
+        v(i) = i
+    }
+
+    cout << " A = " << A << endl;
+    cout << " x = " << x << endl;
+    cout << "A*x = " << A*x << endl;
+}
